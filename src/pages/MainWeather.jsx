fde9e940914bfac6a4b67d7c4935fe2f8b3592b9
matan <matanamz@gmail.com>
2024-05-09 20:42:08 +0300

    adding show more details

--- src/pages/MainWeather.jsx
+++ src/pages/MainWeather.jsx
@@ -2,18 +2,20 @@ import { useEffect, useState } from "react";
 import { HourlyTempCard } from "../cmps/HourlyTempCard";
 import { utilService } from "../services/util.service";
 
-
 export const MainWeather = ({
   weatherData,
   fetchCurrLocationData,
   weeklyForecast,
 }) => {
-
-  const [isSeeMore, setIsSeeMore] = useState(false)
-  console.log(weeklyForecast);
+  const [isSeeMore, setIsSeeMore] = useState(false);
+  console.log(weatherData.forecast.forecastday[0].hour[getCurrentHour()]);
   const desiredHours = [6, 9, 12, 15, 18, 21];
   const icons = [
-    { icon: "temp", title: "real feel", data: weatherData.current.feelslike_c + "\u00B0"  },
+    {
+      icon: "temp",
+      title: "real feel",
+      data: weatherData.current.feelslike_c + "\u00B0",
+    },
     {
       icon: "wind",
       title: "wind",
@@ -30,6 +32,25 @@ export const MainWeather = ({
       data: `${weatherData.current.uv}`,
     },
   ];
+
+  function getCurrentHour() {
+    const date = new Date();
+    const currentHour = date.getHours();
+    return currentHour;
+  }
+
+  function getFilteredForecat() {
+    let currentHour = getCurrentHour();
+    const filteredForecast = [];
+    for (var i = 0; i < 3; i++) {
+      filteredForecast.push(
+        weatherData.forecast.forecastday[0].hour[currentHour]
+      );
+      currentHour += 3;
+      if (currentHour > 24) currentHour = currentHour - 24;
+    }
+    return filteredForecast;
+  }
 
   return (
     <section className="main-weather-sec debug flex">
@@ -59,24 +80,30 @@ export const MainWeather = ({
             />
           </div>
         </div>
-        {isSeeMore ?  null :  <div className="hourly-forecast ">
-          <h4 className="title">TODAYS FORECAST</h4>
-          <div className="hourly-cards-layout flex">
-            {weatherData.forecast.forecastday[0].hour.map((hourData, idx) => {
-              if (desiredHours.includes(idx)) {
-                return <HourlyTempCard key={idx} weatherData={hourData} />;
-              }
-              return null;
-            })}
+        {isSeeMore ? null : (
+          <div className="hourly-forecast ">
+            <h4 className="title">TODAYS FORECAST</h4>
+            <div className="hourly-cards-layout flex">
+              {weatherData.forecast.forecastday[0].hour.map((hourData, idx) => {
+                if (desiredHours.includes(idx)) {
+                  return <HourlyTempCard key={idx} weatherData={hourData} />;
+                }
+                return null;
+              })}
+            </div>
           </div>
-        </div>}
-       
+        )}
 
         <div className="forecast-details relative">
-          <button className="see-more-btn" onClick={() => setIsSeeMore(!isSeeMore)}>{isSeeMore ? "Show less" : "Show more"}</button>
+          <button
+            className="see-more-btn"
+            onClick={() => setIsSeeMore(!isSeeMore)}
+          >
+            {isSeeMore ? "Show less" : "Show more"}
+          </button>
           <h1 className="title">AIR CONDITIONS</h1>
           <div className="air-condition-details-sec flex wrap space-between">
-            { icons.map((icon) => {
+            {icons.map((icon, key) => {
               const iconSrc = `src\\assets\\icons\\weather-icons\\${icon.icon}.png`;
               return (
                 <div className="detail-sec flex ">
@@ -91,22 +118,36 @@ export const MainWeather = ({
           </div>
         </div>
       </div>
-      <div className="weekly-weather-sec flex column">
-        <h3 className="title">7 Day Forecast</h3>
-        <div className="weekly-forcast-form">
-          {weeklyForecast.map((info) => {
-            const iconSrc = `http://openweathermap.org/img/w/${info.weather[0].icon}.png`
-            return (
-              <div className="daily-forecast flex space-between">
-                <h5>{utilService.getDayOfWeek(info.dt)}</h5>
-                <div className="icon-sec flex align-center">
-              <img className="icon" src={iconSrc} alt="" />
-                <h3>{info.weather[0].main}</h3>
+
+      <div className="weekly-forecast-container flex column">
+        {!isSeeMore ? null : (
+          <div className="todays-forecast-sec">
+            <h3 className="title">Today's Forecast</h3>
+            <div className="hourly-day-forecast flex">
+              {getFilteredForecat().map((forecast, idx) => {
+                return <HourlyTempCard key={idx} weatherData={forecast} />;
+              })}
+            </div>
+          </div>
+        )}
+
+        <div className="weekly-weather-sec flex column">
+          <h3 className="title">7 Day Forecast</h3>
+          <div className="weekly-forcast-form">
+            {weeklyForecast.map((info) => {
+              const iconSrc = `http://openweathermap.org/img/w/${info.weather[0].icon}.png`;
+              return (
+                <div className="daily-forecast flex space-between">
+                  <h5>{utilService.getDayOfWeek(info.dt)}</h5>
+                  <div className="icon-sec flex align-center">
+                    <img className="icon" src={iconSrc} alt="" />
+                    <h3>{info.weather[0].main}</h3>
+                  </div>
+                  <h5>18/11</h5>
                 </div>
-                <h5>18/11</h5>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
       </div>
     </section>
